404e69acb9ce5a1cc1f81221008653fc5cc50b84
thomas macintire <thomastrkie@gmail.com>
2020-11-02 13:24:12 -0600

    added more to the job creator, fleshing out the generate mission option as well as adding a fill all fields except name and description fields.

--- JobBoardCreator/MainWindow.xaml.cs
+++ JobBoardCreator/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Newtonsoft.Json;
 using TravellerWiki.Data;
+using ValidationResult = System.Printing.ValidationResult;
 
 namespace JobBoardCreator
 {
@@ -29,6 +30,7 @@ namespace JobBoardCreator
         {
             InitializeComponent();
             Jobs = new List<TravellerJobBoardJob>();
+            missionGeerGeneratorService = new TravellerMissionGeneratorService();
         }
 
         private List<TravellerJobBoardJob> Jobs;
@@ -160,6 +162,11 @@ namespace JobBoardCreator
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
+            GenerateIssueDate();
+        }
+
+        private void GenerateIssueDate()
+        {
             var random = new Random();
             var year = random.Next(83389, 83391);
             var month = random.Next(1, 13);
@@ -167,10 +174,10 @@ namespace JobBoardCreator
 
             IssueDay.Text = Convert.ToString(day);
             IssueMonth.Text = Convert.ToString(month);
-            IssueYear.Text = Convert.ToString(day);
+            IssueYear.Text = Convert.ToString(year);
         }
 
-        private void Button_Click_3(object sender, RoutedEventArgs e)
+        private void GeneateExpiryDate()
         {
             var random = new Random();
             var year = random.Next(83390, 83399);
@@ -179,13 +186,18 @@ namespace JobBoardCreator
 
             ExpiryDay.Text = Convert.ToString(day);
             ExpiryMonth.Text = Convert.ToString(month);
-            ExpiryYear.Text = Convert.ToString(day);
+            ExpiryYear.Text = Convert.ToString(year);
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            GeneateExpiryDate();
         }
 
         private (int x, int y) GenerateSubsector(Random random)
         {
-            var x = random.Next(1, 5);
-            var y = random.Next(1, 5);
+            var x = random.Next(0, 4);
+            var y = random.Next(0, 4);
             return (x, y);
         }
 
@@ -196,44 +208,110 @@ namespace JobBoardCreator
                 //Kendrick
                 new List<(int x, int y)> {(1,1),(6,1),(8,1),(5,2),(7,2),(2,3),(5,3),(6,3),(6,3),(5,4),(7,4),(3,5),(6,5),(7,5),(8,5),(1,6),(5,6),(8,6),(4,7),(6,7),(2,8),(3,8),(4,8),(1,9),(2,9),(4,9),(6,9),(2,10),(6,10),(7,10)},
                 //Wynona
-                new List<(int x, int y)>{}, 
+                new List<(int x, int y)>{ (1,1),(6,1),(8,1),(1,2),(8,2),(4,3),(6,4),(7,4),(2,5),(3,5),(7,6),(2,7),(5,7),(8,7),(4,8),(6,8),(8,8),(1,10),(3,10),(4,9),(6,10),(7,10)}, 
                 //Olivehurst
-                new List<(int x, int y)>{}, 
+                new List<(int x, int y)>
+                {
+                    (1,1),(2,1),(6,1),(1,2),(7,2),(8,2),(1,3),(2,3),(4,3),(5,3),(6,3),(8,3),(6,4),(2,5),(5,5),(6,5),
+                    (8,5),(2,6),(4,6),(3,7),(7,7),(4,7),(4,8),(7,8),(7,9),(3,10),(7,10)
+                }, 
                 //Payette
-                new List<(int x, int y)>{},
+                new List<(int x, int y)>
+                {
+                    (5,1),(6,1),(6,2),(2,3),(3,4),(6,4),(1,5),(2,5),(5,6),
+                    (7,6),(8,6),(3,7),(6,7),(8,7),(4,8),(5,8),(3,9),(4,9),
+                    (8,9),(2,9),(6,10),(8,10),
+                },
             },
             //Second Row
             {
                 //Oren Wastes 1
-                new List<(int x, int y)>{},
+                new List<(int x, int y)>
+                {
+                    (2,1),(4,1),(1,2),(3,2),(5,2),(7,2),(1,3),(8,2),(4,3),(5,3),
+                    (6,3),(7,3),(8,3),(1,4),(2,4),(3,4),(4,4),(6,4),(8,4),(4,5),
+                    (5,5),(6,5),(8,5),(3,6),(4,6),(7,6),(1,7),(2,7),(4,7),(7,7),
+                    (8,7),(1,8),(2,8),(5,8),(6,8),(7,8),(6,9),(8,9),(5,10),(6,10),
+                    (8,10)
+                },
                 //Oren Wastes 2
-                new List<(int x, int y)>{},
+                new List<(int x, int y)>
+                {
+                    (1,1),(3,2),(4,2),(2,3),(4,3),(5,3),(8,3),(7,5),(6,5),(4,6),
+                    (4,8),(4,9),(7,9),(5,10),
+                },
                 //Oren Wastes 3
-                new List<(int x, int y)>{},
+                new List<(int x, int y)>{
+                    (2,1),(7,1),(1,2),(5,2),(8,2),(2,3),(5,3),(7,3),(8,3),(3,4),
+                    (5,4),(6,4),(3,5),(7,5),(1,6),(6,6),(1,7),(4,7),(5,7),(1,8),
+                    (3,8),(5,8),(7,8),(1,9),(4,9),(6,9),(5,10),(6,10),
+
+                },
                 //Oren Wastes 4
-                new List<(int x, int y)>{},
+                new List<(int x, int y)>
+                {
+                    (2,1),(3,1),(3,2),(7,2),(6,3),(5,4),(4,5),(1,6),(6,7),(7,7),
+                    (4,9),(5,9),(6,9),(7,9),(7,10),(8,10)
+                },
             },
             //Third row
             {
                 //Doland
-                new List<(int x, int y)>{},
+                new List<(int x, int y)>
+                {
+                    (5,1),(6,1),(3,2),(5,2),(7,2),(3,3),(5,3),(7,3),(3,4),(5,4),
+                    (2,4),(6,4),(2,5),(8,5),(1,7),(2,7),(3,7),(5,7),(6,6),(8,7),
+                    (3,8),(5,8),(8,8),(2,9),(3,9),(4,9),(6,9),(4,10),(6,10),(8,10),
+                },
                 //New Islands
-                new List<(int x, int y)>{},
+                new List<(int x, int y)>
+                {
+                    (1,1),(8,1),(7,2),(3,3),(5,3),(1,5),(7,5),(1,6),(7,6),(6,6),
+                    (5,7),(1,10),(2,10),(6,9),(8,8),
+                },
                 //Old Islands
-                new List<(int x, int y)>{},
+                new List<(int x, int y)>
+                {
+                    (2,2),(3,3),(5,3),(7,2),(1,4),(7,4),(8,3),(3,5),(6,5),(7,5),
+                    (4,6),(1,9),
+                },
                 //Stacyville
-                new List<(int x, int y)>{},
+                new List<(int x, int y)>
+                {
+                    (2,1),(5,1),(7,2),(1,3),(3,3),(4,3),(7,3),(3,4),(6,3),(7,4),
+                    (5,5),(7,5),(3,6),(2,6),(2,7),(3,7),(8,7),(7,8),(6,8),(8,8),
+                    (8,9),(1,9),(2,9),(2,10),(4,9),(6,10),
+                },
             },
             //Fourth Row
             {
                 //Intercourse
-                new List<(int x, int y)>{},
+                new List<(int x, int y)>
+                {
+                    (2,2),(1,3),(8,3),(2,4),(4,5),(7,6),(8,6),(7,7),(4,7),(7,8),
+                    (2,8),(7,9),(2,9),(1,10),(2,10),(6,10),(8,10),
+                },
                 //Citra
-                new List<(int x, int y)>{},
+                new List<(int x, int y)>
+                {
+                    (2,1),(4,1),(6,1),(7,1),(3,2),(4,2),(6,2),(3,3),(5,3),(3,4),
+                    (4,4),(7,4),(1,5),(4,5),(5,5),(6,5),(7,6),(1,7),(5,8),(4,9),
+                    (1,10),
+                },
                 //Wallwern
-                new List<(int x, int y)>{},
+                new List<(int x, int y)>
+                {
+                    (1,1),(2,1),(4,2),(6,1),(5,3),(6,3),(7,3),(1,4),(3,4),(1,5),
+                    (3,5),(3,6),(2,6),(3,7),(5,7),(8,6),(4,8),(2,9),(3,9),(4,9),
+                    (6,8),(7,8),(4,10),(5,10),
+                },
                 //Zaleski
-                new List<(int x, int y)>{},
+                new List<(int x, int y)>
+                {
+                    (4,1),(7,1),(3,2),(8,2),(6,3),(8,3),(5,4),(6,4),(7,4),(8,4),
+                    (2,5),(3,5),(4,5),(6,5),(7,5),(3,6),(4,6),(6,7),(7,8),(8,7),
+                    (5,9),
+                },
             },
 
 
@@ -248,16 +326,89 @@ namespace JobBoardCreator
 
         private void Button_Click_4(object sender, RoutedEventArgs e)
         {
+            GenerateXY();
+        }
+
+        private void GenerateXY()
+        {
             var random = new Random();
             var subsector = GenerateSubsector(random);
 
             var planet = GeneratePlanet(subsector, random);
+            subsector = (subsector.x + 1, subsector.y + 1);
 
             SenderSubsectorX.Text = Convert.ToString(subsector.x);
             SenderSubsectorY.Text = Convert.ToString(subsector.y);
 
             SenderPlanetX.Text = Convert.ToString(planet.x);
             SenderPlanetY.Text = Convert.ToString(planet.y);
+        }
+
+        private TravellerMissionGeneratorService missionGeerGeneratorService;
+
+        private void GenerateMission()
+        {
+            var mission = missionGeerGeneratorService.GenerateTravellerMission();
+            Mission.Text = mission.Mission;
+            Ally.Text = mission.Ally;
+            Enemy.Text = mission.Enemy;
+            Patron.Text = mission.Patron;
+            PatronQuirk.Text = mission.PatronQuirk;
+            Target.Text = mission.Target;
+            TargetQuirk.Text = mission.TargetQuirk;
+            Opposition.Text = mission.Opposition;
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            GenerateMission();
+        }
+
+        private void GeneratePayment()
+        {
+            var payment = new Random();
+            Payment.Text = $"{payment.Next(1000,1000000000)}Cr";
+        }
+
+        private void GenerateCurrentActives(int numberAccepted)
+        {
+            var rand = new Random();
+            NumberActive.Text = Convert.ToString(rand.Next(0, numberAccepted));
+        }
+
+        private int GenerateCurrentAccepts(int maxAccepts)
+        {
+            var rand = new Random();
+            var numb = rand.Next(0, maxAccepts);
+            StartingAccepted.Text = Convert.ToString(numb);
+
+            return numb;
+        }
+
+        private int GenerateMaxAccepts()
+        {
+            var rand = new Random();
+            var numb = rand.Next(1, 1025);
+            MaximumAccepted.Text = Convert.ToString(numb);
+
+            return numb;
+        }
+
+        private void GenerateAllActives()
+        {
+            var max = GenerateMaxAccepts();
+            var currentAccepts = GenerateCurrentAccepts(max);
+            GenerateCurrentActives(currentAccepts);
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            GenerateMission();
+            GenerateXY();
+            GenerateIssueDate();
+            GeneateExpiryDate();
+            GeneratePayment();
+            GenerateAllActives();
         }
     }
 }
---
+++ TravellerWiki/Data/TravellerMission.cs
@@ -0,0 +1,26 @@
+﻿namespace JobBoardCreator
+{
+    public class TravellerMission
+    {
+        public string Ally { get; set; }
+        public string Enemy { get; set; }
+        public string Patron { get; set; }
+        public string PatronQuirk { get; set; }
+        public string Target { get; set; }
+        public string TargetQuirk { get; set; }
+        public string Opposition { get; set; }
+        public string Mission { get; set; }
+
+        public TravellerMission(string ally, string enemy, string patron, string patronQuirk, string target, string targetQuirk, string opposition, string mission)
+        {
+            Ally = ally;
+            Enemy = enemy;
+            Patron = patron;
+            PatronQuirk = patronQuirk;
+            Target = target;
+            TargetQuirk = targetQuirk;
+            Opposition = opposition;
+            Mission = mission;
+        }
+    }
+}
